6732882bb6f30de9d7a5e9fec77b684cc9d8690a
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-07 23:01:47 -0700

    remove unused variable and Component import

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 function Video() {
-  let vidData = ''
 
   return (
     <div className="container camera-feed">
       <div className="row">
         <div className="col-sm-6 col-sm-offset-3">
-          <img src='https://65.128.75.135:42222/camera' />
+          <img alt="camera feed" src='https://65.128.75.135:42222/camera' />
         </div>
       </div>
     </div>
