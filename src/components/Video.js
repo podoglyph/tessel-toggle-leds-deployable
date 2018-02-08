5663c1cddeb84af1c12ba0ecb8dff9c28fd2e12c
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-07 22:36:40 -0700

    change to https

--- src/components/Video.js
+++ src/components/Video.js
@@ -7,7 +7,7 @@ function Video() {
     <div className="container camera-feed">
       <div className="row">
         <div className="col-sm-6 col-sm-offset-3">
-          <img src='http://65.128.75.135:42222/camera' />
+          <img src='https://65.128.75.135:42222/camera' />
         </div>
       </div>
     </div>
